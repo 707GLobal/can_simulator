a18f4659b2f7ade000dd45e8db297cb743a3b2dd
707GLobal <2578034340@qq.com>
2026-07-09 17:46:44 +0800

    加入launch文件

--- src/can_simulator/setup.py
+++ src/can_simulator/setup.py
@@ -1,3 +1,4 @@
+import glob
 from setuptools import find_packages, setup
 
 package_name = 'can_simulator'
@@ -10,6 +11,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch',
+            glob.glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
